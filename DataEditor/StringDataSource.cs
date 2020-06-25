88e3e444637d540c0d0cddb6d7f190be17f67ec3
AlenX23 <alenx6123@gmail.com>
2020-06-25 13:13:42 +0300

    1

---
+++ DataEditor/StringDataSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace WpfApp3
+{
+    public class Student
+    {
+        public Student() { }
+        public Student(string name, string surname, string group, int course)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Group = group;
+            this.Course = course;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public int Course;
+
+        public int Proverka
+        {
+            get => this.Course;
+            set
+            {
+                if (value > 5) value = 5;
+                else if (value < 1) value= 1;
+                this.Course = value;
+            }
+        }
+    }
+
+    [Serializable]
+    public class StringDataSource
+    {
+        public ObservableCollection<Student> data = new ObservableCollection<Student>();
+    }
+}
---
+++ DataEditor/Window1.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using Microsoft.Win32;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        StringDataSource _dataSource;
+        public StringDataSource dataSource
+        {
+            get
+            {
+                return _dataSource;
+            }
+            set
+            {
+                _dataSource = value;
+                listBox.ItemsSource = value.data;
+            }
+        }
+
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            saveFileDialog.DefaultExt = ".xml";
+            openFileDialog.DefaultExt = ".xml";
+            saveFileDialog.Filter = "XML Files|*.xml|All Files|*.*";
+            openFileDialog.Filter = "XML Files|*.xml|All Files|*.*";
+
+            dataSource = new StringDataSource();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewStudent newStudentDialog = new NewStudent();
+            //newStudentDialog.ShowDialog();
+            if (newStudentDialog.ShowDialog() == true)
+            {
+                dataSource.data.Add(newStudentDialog.Student);
+            }
+
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataSource.data.Remove(listBox.SelectedItem as Student);
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var serializer = new XmlSerializer(typeof(StringDataSource));
+
+                using (XmlWriter fs = XmlWriter.Create(saveFileDialog.FileName))
+                {
+                    serializer.Serialize(fs, dataSource);
+
+                    Debug.WriteLine("Объект сериализован");
+                }
+            }
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var serializer = new XmlSerializer(typeof(StringDataSource));
+                using (XmlReader fs = XmlReader.Create(openFileDialog.FileName))
+                {
+                    
+                    dataSource = serializer.Deserialize(fs) as StringDataSource;
+                }
+            }
+        }
+    }
+}
